d3226939e41dba7819500d3ed6615ac8caea63b6
Fernando Alfaro <alfarofernando937@gmail.com>
2026-08-23 20:43:58 -0600

    Actualizar build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,26 @@
 plugins {
-    id("com.android.application") version "8.5.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.dontaxi.app"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.dontaxi.app"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
 }
