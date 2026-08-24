89cdcfd79def436005ae5901d63be8d02d688b60
Fernando Alfaro <alfarofernando937@gmail.com>
2026-08-23 22:43:18 -0600

    Update build.gradle.kts to new plugin syntax
    Removed old Android configuration and added new plugin versions.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,4 @@
-{
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+plugins {
+    id("com.android.application") version "8.7.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
 }
-
-android {
-    namespace = "com.dontaxi.app"
-    compileSdk = 35
-
-    defaultConfig {
-        applicationId = "com.dontaxi.app"
-        minSdk = 24
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-}
-
-dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
