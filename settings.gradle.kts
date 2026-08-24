9da016030edd845f3c5246c20891d74b87f7b7f3
Fernando Alfaro <alfarofernando937@gmail.com>
2026-08-23 20:14:20 -0600

    Actualizar settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,20 @@
-settings.gradle.kts
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Don-Taxi"
+
+include(":app")
+configurar gradle
